477ebdf70d69e2c3e8c41365f8f26cad086e09c2
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2024-06-16 02:50:07 -0300

    finished location method channel

--- android/app/src/main/kotlin/com/example/method_channel_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/method_channel_sample/MainActivity.kt
@@ -1,5 +1,47 @@
 package com.example.method_channel_sample
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example/native"
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "getLocation") {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+                } else {
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location: Location? ->
+                            if (location != null) {
+                                result.success(mapOf("latitude" to location.latitude, "longitude" to location.longitude))
+                            } else {
+                                result.error("UNAVAILABLE", "Location not available", null)
+                            }
+                        }
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,47 @@
-import Flutter
 import UIKit
+import Flutter
+import CoreLocation
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, CLLocationManagerDelegate {
+  var locationManager: CLLocationManager!
+  var result: FlutterResult?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let locationChannel = FlutterMethodChannel(name: "com.example/native",
+                                               binaryMessenger: controller.binaryMessenger)
+    locationChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      if call.method == "getLocation" {
+        self.result = result
+        self.startLocation()
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func startLocation() {
+    locationManager = CLLocationManager()
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
+    locationManager.startUpdatingLocation()
+  }
+
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    if let location = locations.first {
+      result?(["latitude": location.coordinate.latitude, "longitude": location.coordinate.longitude])
+      locationManager.stopUpdatingLocation()
+    }
+  }
+
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    result?(FlutterError(code: "UNAVAILABLE", message: "Location not available", details: nil))
   }
 }
